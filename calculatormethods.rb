ee880345c9896e7722f90c31d12adbb5dec9ff35
Becky Bell <rlbell@alum.bu.edu>
2014-06-02 14:05:30 -0400

    added exponent and square root functions

--- calculatormethods.rb
+++ calculatormethods.rb
@@ -1,15 +1,20 @@
 # Calculator welcome message and request for mathmatical operation
 puts "Welcome to your calculator!"
 puts
-puts "Please choose an operation:  addition, subtraction, multiplication, division"
+puts "Please choose an operation:  addition, subtraction, multiplication, division, exponent, square root"
 puts
 
 # Selection of operator by user and request for two numbers to compute
 calculator_operation = gets.chomp
-puts "You've chosen #{calculator_operation}. Please enter your first number"
-input_number1 = gets.chomp.to_i
-puts "Please enter your second number"
-input_number2 = gets.chomp.to_i
+	if calculator_operation == "square root"
+		puts "You've chosen #{calculator_operation}. Please enter a number"
+		input_number1 = gets.chomp.to_i
+	else
+		puts "You've chosen #{calculator_operation}. Please enter your first number"
+		input_number1 = gets.chomp.to_i
+		puts "Please enter your second number"
+		input_number2 = gets.chomp.to_i
+	end
 
 # Addition method that computes two numbers entered by user then ends program
 def addition(input_number1, input_number2)
@@ -35,8 +40,21 @@ end
 # Division method that computes two numbers entered by user then ends program
 def division(input_number1, input_number2)
 	quotient = input_number1 / input_number2
-	puts "The quotient of #{input_number1} and #{input_number2} is #{quotient}."
+	puts "The quotient of #{input_number1} and #{input_number2}th is #{quotient}."
 	puts "Thank you for using the calculator. Goodbye!"
+end
+
+# Exponent method that multiplies the first number the number of times given by the second number then ends program
+def exponent(input_number1)
+	exponent = input_number1 ** input_number2
+	puts "The exponent of #{input_number1} to the power of #{input_number2} is #{exponent}."
+	puts "Thank you for using the calculator. Goodbye!"
+end
+
+def square_root (input_number1)
+	square = Math.sqrt(input_number1)
+	puts "The square root of #{input_number1} is #{square}"
+
 end
 
 # Cases that call different math operations after user selects initial math operation
@@ -49,6 +67,10 @@ case calculator_operation
 	    multiplication(input_number1, input_number2)
 	when "division"
 	    division(input_number1, input_number2)
+	when "exponent"
+			exponent(input_number1, input_number2)
+	when "square root"
+		square_root(input_number1)
 end
 
 
